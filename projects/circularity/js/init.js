5e39911b01aefc68e6c820610fc76876a615787b
DHiggs2919 <khiggerson2919@gmail.com>
2022-12-06 16:54:04 +0000

    refractor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -47,21 +47,17 @@ var init = function (window) {
             function update() {
                 // TODO 4 : Update the circle's position //
            
-                physikz.updatePosition(circles[0]);
-                physikz.updatePosition(circles[1]);
-                physikz.updatePosition(circles[2]);
-                physikz.updatePosition(circles[3]);
-                physikz.updatePosition(circles[4]);
+                for (var i = 0; i < circles.length; i++){
+                    var eachCircles = circles[i]
+                   physikz.checkCirclePosition(eachCircles);
+                   game.checkCirclePosition(eachCircles);
+                }
             }
 
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-            game.checkCirclePosition(circles[0]);
-            game.checkCirclePosition(circles[1]);
-            game.checkCirclePosition(circles[2]);
-            game.checkCirclePosition(circles[3]);
-            game.checkCirclePosition(circles[4]);
-           
+          
+           //was deleted
 
             // TODO 9 : Iterate over the array
            
